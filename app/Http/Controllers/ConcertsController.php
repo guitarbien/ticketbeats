12c409e7f955456d3fe617968bdef4e08bbf48bc
Ricky <guitarbien@gmail.com>
2017-01-06 17:08:31 +0800

    controller use $id to select from the Concert table

--- app/Http/Controllers/ConcertsController.php
+++ app/Http/Controllers/ConcertsController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Concert;
 use Illuminate\Http\Request;
 
 class ConcertsController extends Controller
 {
-    public function show()
+    public function show($id)
     {
+        $concert = Concert::find($id);
         return view('concerts.show', ['concert' => $concert]);
     }
 }
