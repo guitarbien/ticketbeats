e8aecedac8793823b9b57889ff9572922c079e6d
guitarbien <guitarbien@gmail.com>
2017-01-07 23:38:09 +0800

    QueryScope：published()

--- app/Concert.php
+++ app/Concert.php
@@ -9,6 +9,11 @@ class Concert extends Model
     protected $guarded = [];
     protected $dates   = ['date'];
 
+    public function scopePublished()
+    {
+        return Concert::whereNotNull('published_at');
+    }
+
     public function getFormattedDateAttribute()
     {
         return $this->date->format('F j, Y');
