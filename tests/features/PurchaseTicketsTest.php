294621c5a0dfd90adea8fc058d7ce677106c70a6
guitarbien <guitarbien@gmail.com>
2017-01-09 00:23:29 +0800

    同樣寫了訂單之後也要寫ticket明細，這邊應比照訂單 做order和ticket的關聯

--- tests/features/PurchaseTicketsTest.php
+++ tests/features/PurchaseTicketsTest.php
@@ -37,7 +37,7 @@ class PurchaseTicketsTest extends TestCase
 
         // Make sure that an order exists for this customer
         $order = $concert->orders()->where('email', 'john@example.com')->first();
-        $this->assertEquals(3, $order->tickets->count());
+        $this->assertEquals(3, $order->tickets()->count());
     }
 
 }
