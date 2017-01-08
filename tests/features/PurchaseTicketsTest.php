9d784865b2e908db1f7427698cacf0985d09e640
guitarbien <guitarbien@gmail.com>
2017-01-08 21:22:56 +0800

    1. 設定票價 2. 前端可能直接 form post 或是 ajax call, 將參數組成 json 傳往 API 3. 驗證買三張票應為9750、從資料庫撈出訂單應有三張票

--- tests/features/PurchaseTicketsTest.php
+++ tests/features/PurchaseTicketsTest.php
@@ -1,24 +1,34 @@
 <?php
 
+use App\Concert;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class PurchaseTicketsTest extends TestCase
 {
+    use DatabaseMigrations;
+
     public function test_使用者可以購票()
     {
         // Arrange
         // Create a concert
-
+        $concert = factory(Concert::class)->create(['ticket_price' => 3250]);
 
         // Action
         // Purchase concert tickets
-
+        $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'email'           => 'john@example.com',
+            'ticket_quantity' => 3,
+            'payment_token'   => $paymentGateway->getValidTestToken(),
+        ]);
 
         // Assert
         // Make sure the customer was charged the correct amount
+        $this->assertEquals(9750, $paymentGateway->totalCharges());
         // Make sure that an order exists for this customer
+        $order = $concert->orders()->where('email', 'john@example.com')->first();
+        $this->assertEquals(3, $order->tickets->count());
     }
 
 }
