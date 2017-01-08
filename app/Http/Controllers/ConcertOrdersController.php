3abe3b8e44e4915df792a24c4b6a26be1a7df598
guitarbien <guitarbien@gmail.com>
2017-01-08 22:57:42 +0800

    把剛剛 paymentGatewayTest 裡面做的事情在 controller 中再做一次
    補上token和金額的邏輯
    
    基本上在這邊dd()來看 totalCharges() 得到的會是9750沒錯，但是測試時還是得到0，fail

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Billing\FakePaymentGateway;
+use App\Concert;
 use Illuminate\Http\Request;
 
 class ConcertOrdersController extends Controller
 {
-    public function store()
+    public function store($concertId)
     {
+        $concert = Concert::find($concertId);
+
+        $amount = request('ticket_quantity') * $concert->ticket_price;
+        $token  = request('payment_token');
+
+        $paymentGateway = new FakePaymentGateway;
+        $paymentGateway->charge($amount, $token);
+
+        // dd($paymentGateway->totalCharges());
+
         return response()->json([], 201);
     }
 }
