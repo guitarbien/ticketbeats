38f330b43767de6263409235a76d164c890430d7
guitarbien <guitarbien@gmail.com>
2017-01-08 21:08:31 +0800

    TestCase：使用者可以購票

---
+++ tests/features/PurchaseTicketsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class PurchaseTicketsTest extends TestCase
+{
+    public function test_使用者可以購票()
+    {
+        // Arrange
+        // Create a concert
+
+
+        // Action
+        // Purchase concert tickets
+
+
+        // Assert
+        // Make sure the customer was charged the correct amount
+        // Make sure that an order exists for this customer
+    }
+
+}
