f573d180206417a75b03eb86d1a544011db2dd26
Ricky <guitarbien@gmail.com>
2017-01-10 15:19:43 +0800

    (多補上測試 下單成功回傳物件應為not null)

--- tests/features/PurchaseTicketsTest.php
+++ tests/features/PurchaseTicketsTest.php
@@ -53,6 +53,7 @@ class PurchaseTicketsTest extends TestCase
 
         // Make sure that an order exists for this customer
         $order = $concert->orders()->where('email', 'john@example.com')->first();
+        $this->assertNotNull($order);
         $this->assertEquals(3, $order->tickets()->count());
     }
 
