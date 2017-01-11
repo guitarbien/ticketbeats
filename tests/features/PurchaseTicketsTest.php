33a0d22ebc05674197a4dc9fee4129610e291a7b
Ricky <guitarbien@gmail.com>
2017-01-11 17:07:56 +0800

    TestCase：不能超量購買 下單api回傳http code 422 訂單不會寫入 剩餘票券數量不變

--- tests/features/PurchaseTicketsTest.php
+++ tests/features/PurchaseTicketsTest.php
@@ -72,6 +72,26 @@ class PurchaseTicketsTest extends TestCase
         $this->assertEquals(0, $this->paymentGateway->totalCharges());
     }
 
+    public function test_不能超量購買()
+    {
+        $concert = factory(Concert::class)->states('published')->create();
+        $concert->addTickets(50);
+
+        $this->orderTickets($concert, [
+            'email'           => 'john@example.com',
+            'ticket_quantity' => 51,
+            'payment_token'   => $this->paymentGateway->getValidTestToken(),
+        ]);
+
+        $this->assertResponseStatus(422);
+
+        $order = $concert->orders()->where('email', 'john@example.com')->first();
+        $this->assertNull($order);
+
+        $this->assertEquals(0, $concert->orders()->count());
+        $this->assertEquals(50, $concert->ticketsRemaining());
+    }
+
     public function test_若付款失敗則不會產生訂單()
     {
         $concert = factory(Concert::class)->states('published')->create(['ticket_price' => 3250]);
