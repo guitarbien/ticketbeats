822f4a7c272502dd5c69878090976ef87af32af1
Ricky <guitarbien@gmail.com>
2017-01-12 13:25:58 +0800

    以 ordersFor() 來檢查該客人的訂單數量

--- app/Concert.php
+++ app/Concert.php
@@ -40,6 +40,11 @@ class Concert extends Model
         return $this->orders()->where('email', $customerEmail)->count() > 0;
     }
 
+    public function ordersFor($customerEmail)
+    {
+        return $this->orders()->where('email', $customerEmail)->get();
+    }
+
     public function tickets()
     {
         return $this->hasMany(Ticket::class);
