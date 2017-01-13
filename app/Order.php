28173e9e4b0a6b166c0bda188174b185dd6560ed
Ricky <guitarbien@gmail.com>
2017-01-13 17:29:44 +0800

    但order並不知道 $this->concert 是指什麼 所以要設定 order belongs to concert

--- app/Order.php
+++ app/Order.php
@@ -8,6 +8,11 @@ class Order extends Model
 {
     protected $guarded = [];
 
+    public function concert()
+    {
+        return $this->belongsTo(Concert::class);
+    }
+
     public function tickets()
     {
         return $this->hasMany(Ticket::class);
