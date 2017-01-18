25e84adf39396548c527983df1a70b90ee6b995b
Ricky <guitarbien@gmail.com>
2017-01-18 14:24:12 +0800

    TestCase：驗證總金額 將金額計算的邏輯歸納在一個 domain object 中
    原本金額的計算是依賴在 Tickets 物件並直接sum()
    我們想要有一個 mothod 可以呼叫，而且要有一個物件來操作他
    使用 Reservation 這個詞把 tickets 保留起來

---
+++ tests/unit/Billing/ReservationTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Concert;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class ReservationTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_計算總金額()
+    {
+        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(3);
+        $tickets = $concert->findTickets(3);
+
+        $reservation = new Reservation($tickets);
+
+        $this->assertEquals(3600, $reservation->totalCost());
+    }
+}
