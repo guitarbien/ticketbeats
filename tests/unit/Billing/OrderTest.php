4eebbf5b58fb5e9803bea480f693808e84c9c40f
Ricky <guitarbien@gmail.com>
2017-01-18 10:17:43 +0800

    TestCase： 直接用票券和email就可以建立訂單，不需透過concert啟動 5張票買3張剩2張

--- tests/unit/Billing/OrderTest.php
+++ tests/unit/Billing/OrderTest.php
@@ -10,6 +10,19 @@ class OrderTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function test_用票券和email建立訂單()
+    {
+        $concert = factory(Concert::class)->states('published')->create(['ticket_price' => 1200])->addTickets(5);
+        $this->assertEquals(5, $concert->ticketsRemaining());
+
+        $order = Order::forTickets($concert->findTickets(3), 'jane@example.com');
+
+        $this->assertEquals('jane@example.com', $order->email);
+        $this->assertEquals(3, $order->ticketQuantity());
+        $this->assertEquals(3600, $order->amount);
+        $this->assertEquals(2, $concert->ticketsRemaining());
+    }
+
     public function test_轉換成array()
     {
         $concert = factory(Concert::class)->states('published')->create(['ticket_price' => 1200])->addTickets(5);
