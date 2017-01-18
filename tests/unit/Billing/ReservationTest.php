e8daa09c7da8eedebab79512f61a06819821e841
Ricky <guitarbien@gmail.com>
2017-01-18 15:13:46 +0800

    單獨看金額計算的class和測試的話 可以觀察出Reservation totalCost()其實並不需要經由資料庫產生，只要根據進來的物件去sum price 即可 這邊使用 (object) 讓後面可以符合 Collection 的操作

--- tests/unit/Billing/ReservationTest.php
+++ tests/unit/Billing/ReservationTest.php
@@ -1,19 +1,20 @@
 <?php
 
-use App\Concert;
 use App\Reservation;
+use App\Ticket;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class ReservationTest extends TestCase
 {
-    use DatabaseMigrations;
-
     public function test_計算總金額()
     {
-        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(3);
-        $tickets = $concert->findTickets(3);
+        $tickets = collect([
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+        ]);
 
         $reservation = new Reservation($tickets);
 
