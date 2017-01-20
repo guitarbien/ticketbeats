3acfe978ed740ae8b0ecf33f11453b32543490e2
Ricky <guitarbien@gmail.com>
2017-01-20 15:59:40 +0800

    TestCase：票券能被保留 Ticket增加reserve_at的欄位來判別是否已經被保留

--- tests/unit/Billing/TicketTest.php
+++ tests/unit/Billing/TicketTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Concert;
+use App\Ticket;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -8,6 +9,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 class TicketTest extends TestCase
 {
     use DatabaseMigrations;
+
+    public function test_票券能被保留()
+    {
+        $ticket = factory(Ticket::class)->create();
+        $this->assertNull($ticket->reservt_at);
+
+        $ticket->reserve();
+
+        $this->assertNotNull($ticket->fresh()->reserve_at);
+    }
 
     public function test_票券可以被釋出()
     {
