ebc2bf4221677239bed3f1a15d94bd2aa2b0121c
Ricky <guitarbien@gmail.com>
2017-01-24 18:01:21 +0800

    把重複的參數重複縮減到只有一個物件，還可以做的更精簡： 直接由 $reservation 來產生訂單

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -38,7 +38,7 @@ class ConcertOrdersController extends Controller
             $this->paymentGateway->charge($reservation->totalCost(), request('payment_token'));
 
             // Create an order for those tickets
-            $order = Order::fromReservation($reservation);
+            $order = $reservation->complete();
 
             return response()->json([
                 'email'           => $order->email,
