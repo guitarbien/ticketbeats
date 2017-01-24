fb6a0468b45630cfa7a0e203941b706c2026f816
Ricky <guitarbien@gmail.com>
2017-01-24 18:20:12 +0800

    give up PHP 5.6

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -28,15 +28,13 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 
     protected function disableExceptionHandling()
     {
-        $handler = new class extends Handler {
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
             public function __construct() {}
             public function report(Exception $e) {}
             public function render($request, Exception $e)
             {
                 throw $e;
             }
-        };
-
-        $this->app->instance(ExceptionHandler::class, $handler);
+        });
     }
 }
