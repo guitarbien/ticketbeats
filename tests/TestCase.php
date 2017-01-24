f686ecf24271794efb4d1e204fd597efcbe2cd09
Ricky <guitarbien@gmail.com>
2017-01-24 18:16:12 +0800

    不使用 Anonymous classes ，測試 PHP 5.6

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -28,13 +28,15 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 
     protected function disableExceptionHandling()
     {
-        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+        $handler = new class extends Handler {
             public function __construct() {}
             public function report(Exception $e) {}
             public function render($request, Exception $e)
             {
                 throw $e;
             }
-        });
+        };
+
+        $this->app->instance(ExceptionHandler::class, $handler);
     }
 }
