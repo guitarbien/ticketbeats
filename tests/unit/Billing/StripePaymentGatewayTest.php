345c64c5e46ed274eabc7e00c6e45784c80864f3
Ricky <guitarbien@gmail.com>
2017-01-25 12:04:47 +0800

    實際使用 Stripe 的sdk 來產生token
    config/services.php 內已經有定義了 stripe 的區塊(因為laravel cashier也是用stripe)，會去讀 env
    所以把 secret 和 key 都放在 .env 內，如此就不會上傳到外部 (有 .gitignore)

--- tests/unit/Billing/StripePaymentGatewayTest.php
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Billing\StripePaymentGateway;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -9,9 +10,23 @@ class StripePaymentGatewayTest extends TestCase
     public function test_以合法token付款成功()
     {
         // Create a new Stripe paymentGateway
+        $paymentGateway = new StripePaymentGateway;
+
+        // get real token
+        $token = \Stripe\Token::create([
+            "card" => [
+                // give fake card info
+                "number"    => "4242424242424242",
+                "exp_month" => 1,
+                "exp_year"  => date('Y') + 1,
+                "cvc"       => "123",
+            ]
+        ], ['api_key' => config('services.stripe.secret')])->id;
 
         // Create a new charge with some amount using a valid token
+        $paymentGateway->charge(2500, $token);
 
         // Verify that the charge was completed successfully
+        $this->assertEquals(2500, $paymentGateway->totalCharges());
     }
 }
