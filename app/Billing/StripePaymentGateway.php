524d8a66608d1afe9a94dc7878f1042f58018390
Ricky <guitarbien@gmail.com>
2017-01-25 17:32:41 +0800

    Charge::create() 包 try catch，並統一拋出 PaymentFailedException

--- app/Billing/StripePaymentGateway.php
+++ app/Billing/StripePaymentGateway.php
@@ -3,6 +3,7 @@
 namespace App\Billing;
 
 use Stripe\Charge;
+use Stripe\Error\InvalidRequest;
 
 class StripePaymentGateway implements PaymentGateway
 {
@@ -15,12 +16,16 @@ class StripePaymentGateway implements PaymentGateway
 
     public function charge($amount, $token)
     {
-        Charge::create([
-            "amount"      => $amount,
-            "currency"    => "usd",
-            "source"      => $token,
-            "description" => null,
-        ], ['api_key' => $this->apiKey]);
+        try {
+            Charge::create([
+                "amount"      => $amount,
+                "currency"    => "usd",
+                "source"      => $token,
+                "description" => null,
+            ], ['api_key' => $this->apiKey]);
+        } catch (InvalidRequest $e) {
+            throw new PaymentFailedException;
+        }
     }
 
     public function totalCharges()
