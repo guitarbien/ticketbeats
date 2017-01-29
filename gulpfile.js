4ac67ebf136586ba52a1e5d2e0b322f12a2628ad
guitarbien <guitarbien@gmail.com>
2017-01-29 21:25:49 +0800

    for travis, not running phpunit in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,6 @@ require('laravel-elixir-vue-2');
 elixir((mix) => {
     mix.less('app.less')
        .webpack('app.js')
-       .version(['css/app.css', 'js/app.js'])
-       .phpUnit();
+       .version(['css/app.css', 'js/app.js']);
+       // .phpUnit();
 });
