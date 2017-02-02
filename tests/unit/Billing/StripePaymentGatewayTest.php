b93a8e7f7ad9a7b9d6c750ba1ca624c6ddaf6556
guitarbien <guitarbien@gmail.com>
2017-02-03 01:00:34 +0800

    StripePaymentGatewayTest 的付款成功測試也直接使用 contract test 裡面的程式

--- tests/unit/Billing/StripePaymentGatewayTest.php
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -11,6 +11,8 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
  */
 class StripePaymentGatewayTest extends TestCase
 {
+    use PaymentGatewayContractTests;
+
     private $lastCharge;
 
     private function lastCharge()
@@ -54,18 +56,6 @@ class StripePaymentGatewayTest extends TestCase
     protected function getPaymentGateway()
     {
         return new StripePaymentGateway(config('services.stripe.secret'));
-    }
-
-    public function test_以合法token付款成功()
-    {
-        $paymentGateway = $this->getPaymentGateway();
-
-        $newCharges = $paymentGateway->newChargesDuring(function ($paymentGateway) {
-            $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
-        });
-
-        $this->assertCount(1, $newCharges);
-        $this->assertEquals(2500, $newCharges->sum());
     }
 
     public function test_以不合法token付款失敗()
