7f4a74906a725d2c43ca225cbc117e4b8dcf103a
Ricky <guitarbien@gmail.com>
2017-02-03 14:06:44 +0800

    StripePaymentGatewayTest 中原本的 helper 都已經搬移到 PaymentGateway 之中了 移除這些程式測試也都能通過

--- tests/unit/Billing/StripePaymentGatewayTest.php
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Billing\PaymentFailedException;
 use App\Billing\StripePaymentGateway;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 /**
  * @group integration
@@ -12,46 +8,6 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 class StripePaymentGatewayTest extends TestCase
 {
     use PaymentGatewayContractTests;
-
-    private $lastCharge;
-
-    private function lastCharge()
-    {
-        return array_first(\Stripe\Charge::all(
-            ["limit" => 1],
-            ['api_key' => config('services.stripe.secret')]
-        )->data);
-    }
-
-    private function newCharges()
-    {
-        return \Stripe\Charge::all(
-            [
-                "limit"         => 1,
-                "ending_before" => $this->lastCharge ? $this->lastCharge->id : null,
-            ],
-            ['api_key' => config('services.stripe.secret')]
-        )->data;
-    }
-
-    private function validToken()
-    {
-        return \Stripe\Token::create([
-            "card" => [
-                // give fake card info
-                "number"    => "4242424242424242",
-                "exp_month" => 1,
-                "exp_year"  => date('Y') + 1,
-                "cvc"       => "123",
-            ]
-        ], ['api_key' => config('services.stripe.secret')])->id;
-    }
-
-    protected function setUp()
-    {
-        parent::setUp();
-        $this->lastCharge = $this->lastCharge();
-    }
 
     protected function getPaymentGateway()
     {
