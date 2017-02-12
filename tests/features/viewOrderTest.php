d860dd58383d647d4bdcf18ec42a604cf3d95777
guitarbien <guitarbien@gmail.com>
2017-02-12 15:02:10 +0800

    TestCase：使用者可以查看訂單確認頁 建立訂單票券等資訊，查看網址並驗證所得到的內容

---
+++ tests/features/viewOrderTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class viewOrderTest extends TestCase
+{
+    public function test_使用者可以查看訂單確認頁()
+    {
+        // create a concert
+        // create a order
+        // create a ticket
+
+        // visit thr order confirmation page
+        // Assert we see the correct order details
+    }
+}
