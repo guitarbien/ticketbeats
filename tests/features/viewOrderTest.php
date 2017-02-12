9ceafbe3f9a06fa1c326429f69a55929a75b4ed1
guitarbien <guitarbien@gmail.com>
2017-02-12 15:06:00 +0800

    先建立資料，執行測試發現 factory 並沒有 order 的設定

--- tests/features/viewOrderTest.php
+++ tests/features/viewOrderTest.php
@@ -1,16 +1,27 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
+use App\Concert;
+use App\Order;
+use App\Ticket;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class viewOrderTest extends TestCase
 {
+    use DatabaseMigrations;
+
     public function test_使用者可以查看訂單確認頁()
     {
         // create a concert
+        $concert = factory(Concert::class)->create();
         // create a order
+        $order = factory(Order::class)->create();
         // create a ticket
+        $ticket = factory(Ticket::class)->create([
+            'concert_id' => $concert->id,
+            'order_id'   => $order->id,
+        ]);
 
         // visit thr order confirmation page
         // Assert we see the correct order details
