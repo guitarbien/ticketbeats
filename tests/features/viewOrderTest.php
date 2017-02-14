0d28c5b85b409dd30859b04b9319ec294fbc4130
Ricky <guitarbien@gmail.com>
2017-02-14 13:01:46 +0800

    驗證其他欄位

--- tests/features/viewOrderTest.php
+++ tests/features/viewOrderTest.php
@@ -3,6 +3,7 @@
 use App\Concert;
 use App\Order;
 use App\Ticket;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -14,12 +15,24 @@ class viewOrderTest extends TestCase
     public function test_使用者可以查看訂單確認頁()
     {
         // create a concert
-        $concert = factory(Concert::class)->create();
+        $concert = factory(Concert::class)->create([
+            'title'         => 'The Red Chord',
+            'subtitle'      => 'with Animosity and Lethargy',
+            'date'          => Carbon::parse('March 12, 2017 8:00pm'),
+            'ticket_price'  => 4250,
+            'venue'         => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city'          => 'Laraville',
+            'state'         => 'ON',
+            'zip'           => '17916',
+        ]);
+
         // create a order
         $order = factory(Order::class)->create([
             'confirmation_number' => 'ORDERCONFIRMATION1234',
             'card_last_four'      => '1881',
             'amount'              => 8500,
+            'email'               => 'john@example.com',
         ]);
         // create tickets
         $ticketA = factory(Ticket::class)->create([
@@ -49,5 +62,15 @@ class viewOrderTest extends TestCase
         $response->assertSee('**** **** **** 1881');
         $response->assertSee('TICKETCODE123');
         $response->assertSee('TICKETCODE456');
+        $response->assertSee('The Red Chord');
+        $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville, ON');
+        $response->assertSee('17916');
+        $response->assertSee('john@example.com');
+
+        $response->assertSee('Sunday, March 12, 2017');
+        $response->assertSee('Doors at 8:00pm');
     }
 }
