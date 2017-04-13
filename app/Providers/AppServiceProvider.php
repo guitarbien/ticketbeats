a1bd97590288fc1664de8880fbb0109cbf5424cd
guitarbien <guitarbien@gmail.com>
2017-04-13 17:42:30 +0800

    同樣建立 HashidsTicketCodeGenerator 時會需要 salt，在 AppServiceProvider 指定讀取 config/app.php 再讀 .env 的設定內容，實際的 salt 隨便給即可

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,6 +33,10 @@ class AppServiceProvider extends ServiceProvider
             return new StripePaymentGateway(config('services.stripe.secret'));
         });
 
+        $this->app->bind(HashidsTicketCodeGenerator::class, function() {
+            return new HashidsTicketCodeGenerator(config('app.ticket_code_salt'));
+        });
+
         $this->app->bind(PaymentGateway::class, StripePaymentGateway::class);
         $this->app->bind(OrderConfirmationNumberGenerator::class, RandomOrderConfirmationNumberGenerator::class);
         $this->app->bind(TicketCodeGenerator::class, HashidsTicketCodeGenerator::class);
