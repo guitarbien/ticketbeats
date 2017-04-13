e356528378855f5493114fe6742e68ab6b4632a4
guitarbien <guitarbien@gmail.com>
2017-04-13 17:26:04 +0800

    rename

--- app/HashidsTicketCodeGenerator.php
+++ app/HashidsTicketCodeGenerator.php
@@ -2,14 +2,13 @@
 
 namespace App;
 
-class HashidTicketCodeGenerator implements TicketCodeGenerator
+class HashidsTicketCodeGenerator implements TicketCodeGenerator
 {
     private $hashids;
 
     public function __construct($salt)
     {
         $this->hashids = new \Hashids\Hashids($salt, 6, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-
     }
 
     public function generateFor($ticket)
--- tests/unit/Billing/HashidsTicketCodeGeneratorTest.php
+++ tests/unit/Billing/HashidsTicketCodeGeneratorTest.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\HashidTicketCodeGenerator;
+use App\HashidsTicketCodeGenerator;
 use App\Ticket;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -10,7 +10,7 @@ class HashidsTicketCodeGeneratorTest extends TestCase
 {
     public function test_票券代碼至少為六位數()
     {
-        $ticketCodeGenerator = new HashidTicketCodeGenerator('testsalt1');
+        $ticketCodeGenerator = new HashidsTicketCodeGenerator('testsalt1');
         $code = $ticketCodeGenerator->generateFor(new Ticket(['id' => 1]));
 
         $this->assertTrue(strlen($code) >= 6);
@@ -18,7 +18,7 @@ class HashidsTicketCodeGeneratorTest extends TestCase
 
     public function test_票券代碼只能有大寫字母()
     {
-        $ticketCodeGenerator = new HashidTicketCodeGenerator('testsalt1');
+        $ticketCodeGenerator = new HashidsTicketCodeGenerator('testsalt1');
         $code = $ticketCodeGenerator->generateFor(new Ticket(['id' => 1]));
 
         $this->assertRegexp('/^[A-Z]+$/', $code);
@@ -26,7 +26,7 @@ class HashidsTicketCodeGeneratorTest extends TestCase
 
     public function test_同一個票券id產生的票券代碼是一樣的()
     {
-        $ticketCodeGenerator = new HashidTicketCodeGenerator('testsalt1');
+        $ticketCodeGenerator = new HashidsTicketCodeGenerator('testsalt1');
 
         $code1 = $ticketCodeGenerator->generateFor(new Ticket(['id' => 1]));
         $code2 = $ticketCodeGenerator->generateFor(new Ticket(['id' => 1]));
@@ -36,7 +36,7 @@ class HashidsTicketCodeGeneratorTest extends TestCase
 
     public function test_不同票券id產生的票券代碼是不同的()
     {
-        $ticketCodeGenerator = new HashidTicketCodeGenerator('testsalt1');
+        $ticketCodeGenerator = new HashidsTicketCodeGenerator('testsalt1');
 
         $code1 = $ticketCodeGenerator->generateFor(new Ticket(['id' => 1]));
         $code2 = $ticketCodeGenerator->generateFor(new Ticket(['id' => 2]));
@@ -46,8 +46,8 @@ class HashidsTicketCodeGeneratorTest extends TestCase
 
     public function test_不同salt產生的代碼是不同的()
     {
-        $ticketCodeGenerator1 = new HashidTicketCodeGenerator('testsalt1');
-        $ticketCodeGenerator2 = new HashidTicketCodeGenerator('testsalt2');
+        $ticketCodeGenerator1 = new HashidsTicketCodeGenerator('testsalt1');
+        $ticketCodeGenerator2 = new HashidsTicketCodeGenerator('testsalt2');
 
         $code1 = $ticketCodeGenerator1->generateFor(new Ticket(['id' => 1]));
         $code2 = $ticketCodeGenerator2->generateFor(new Ticket(['id' => 1]));
