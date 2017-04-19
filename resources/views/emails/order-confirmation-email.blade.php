79d272f542c92bc5b60a03420db83a036fe5b533
guitarbien <guitarbien@gmail.com>
2017-04-19 13:57:43 +0800

    加入正式內容

--- resources/views/emails/order-confirmation-email.blade.php
+++ resources/views/emails/order-confirmation-email.blade.php
@@ -1,1 +1,5 @@
-{{ url("/orders/{$order->confirmation_number}") }}
+<p>Thanks for your order!</p>
+
+<p>You can view your ticket anytime by visiting this URL:</p>
+
+<p><a href="{{ url("/orders/{$order->confirmation_number}") }}">{{ url("/orders/{$order->confirmation_number}") }}</a></p>
