bb27b87595c95d2bf96075a75955ef697ade5563
guitarbien <guitarbien@gmail.com>
2017-05-21 00:26:38 +0800

    TestCase：以不合法資訊登入

--- tests/features/PromoterLoginTest.php
+++ tests/features/PromoterLoginTest.php
@@ -28,4 +28,23 @@ class PromoterLoginTest extends TestCase
         $this->assertTrue(Auth::check());
         $this->assertTrue(Auth::user()->is($user));
     }
+
+    public function test_以不合法資訊登入()
+    {
+        $this->disableExceptionHandling();
+
+        $user = factory(User::class)->create([
+            'email'    => 'jane@example.com',
+            'password' => bcrypt('super-secret-password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email'    => 'jane@example.com',
+            'password' => 'not-the-right-password',
+        ]);
+
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertFalse(Auth::check());
+    }
 }
