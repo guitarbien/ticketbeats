6a5ce7cf20defcd988ced0fec1600238764b43b8
guitarbien <guitarbien@gmail.com>
2017-05-21 00:07:22 +0800

    TestCase：以帳密登入後跳轉到/backstage/concerts

---
+++ tests/features/PromoterLoginTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Support\Facades\Auth;
+
+class PromoterLoginTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_以合法資訊登入()
+    {
+        $this->disableExceptionHandling();
+
+        $user = factory(User::class)->create([
+            'email'    => 'jane@example.com',
+            'password' => bcrypt('super-secret-password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email'    => 'jane@example.com',
+            'password' => bcrypt('super-secret-password'),
+        ]);
+
+        $response->assertRedirect('/backstage/concerts');
+        $this->assertTrue(Auth::check());
+        $this->assertTrue(Auth::user()->is($user));
+    }
+}
