9114fdcd792e6ff321e41d8df9fc23cc0a0b2d03
guitarbien <guitarbien@gmail.com>
2017-05-21 00:20:48 +0800

    加入login實作

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    public function login()
+    {
+        Auth::attempt(request(['email', 'password']));
+        return redirect('/backstage/concerts');
+    }
 }
