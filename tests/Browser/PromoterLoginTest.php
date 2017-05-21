e67582babbec91399842ac6beea9fccb15e6d25b
guitarbien <guitarbien@gmail.com>
2017-05-21 19:19:28 +0800

    加入 laravel dusk test 實際開瀏覽器測試登入成功和登入失敗

---
+++ tests/Browser/PromoterLoginTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class PromoterLoginTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function test_登入成功()
+    {
+        $user = factory(User::class)->create([
+            'email'    => 'jane@example.com',
+            'password' => bcrypt('super-secret-password'),
+        ]);
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->type('email', 'jane@example.com')
+                    ->type('password', 'super-secret-password')
+                    ->press('Log in')
+                    ->assertPathIs('/backstage/concerts');
+        });
+    }
+
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function test_登入失敗()
+    {
+        $user = factory(User::class)->create([
+            'email'    => 'jane@example.com',
+            'password' => bcrypt('super-secret-password'),
+        ]);
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->type('email', 'jane@example.com')
+                    ->type('password', 'wrong-password')
+                    ->press('Log in')
+                    ->assertPathIs('/login')
+                    ->assertSee('credentials do not match');
+        });
+    }
+}
