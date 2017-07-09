869578ac6331f851dfa06be2f935587234c4b016
guitarbien <guitarbien@gmail.com>
2017-07-09 22:21:37 +0800

    test case 管理者可以看到音樂會的列表 管理者可以看到音樂會的列表

--- tests/Feature/Backstage/ViewConcertListTest.php
+++ tests/Feature/Backstage/ViewConcertListTest.php
@@ -2,18 +2,29 @@
 
 namespace Tests\Feature\Backstage;
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
+use App\Concert;
+use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class ViewConcertListTest extends TestCase
 {
+    use DatabaseMigrations;
+
     public function test_一般使用者不能看到管理者的音樂列表頁()
     {
         $response = $this->get('/backstage/concerts');
 
         $response->assertStatus(302);
         $response->assertRedirect('/login');
+    }
+
+    public function test_管理者可以看到音樂會的列表()
+    {
+        $user = factory(User::class)->create();
+        factory(Concert::class, 3)->create(['user_id' => $user->id]);
+
+        $response = $this->actingAs($user)->get('/backstage/concerts');
+        $response->assertStatus(200);
     }
 }
