257a5dbf03d079f1ae82a96bd496c13ed1e928ab
guitarbien <guitarbien@gmail.com>
2017-07-21 00:38:25 +0800

    testcase：補強，音樂會只要發佈了就不能更新

--- tests/Feature/Backstage/EditConcertTest.php
+++ tests/Feature/Backstage/EditConcertTest.php
@@ -168,4 +168,53 @@ class EditConcertTest extends TestCase
             $this->assertEquals(2000, $concert->ticket_price);
         });
     }
+
+    public function test_管理者不能編輯尚未發布的音樂會()
+    {
+        $user = factory(User::class)->create();
+
+        $concert = factory(Concert::class)->states('published')->create([
+            'user_id'                => $user->id,
+            'title'                  => 'old title',
+            'subtitle'               => 'old subtitle',
+            'additional_information' => 'old additional_information',
+            'date'                   => Carbon::parse('2017-01-01 5:00pm'),
+            'venue'                  => 'old venue',
+            'venue_address'          => 'old venue_address',
+            'city'                   => 'old city',
+            'state'                  => 'old state',
+            'zip'                    => '00000',
+            'ticket_price'           => 2000,
+        ]);
+
+        $this->assertTrue($concert->isPublished());
+
+        $response = $this->actingAs($user)->patch("/backstage/concerts/{$concert->id}", [
+            'title'                  => 'new title',
+            'subtitle'               => 'new subtitle',
+            'additional_information' => 'new additional_information',
+            'date'                   => '2018-12-12',
+            'time'                   => '8:00pm',
+            'venue'                  => 'new venue',
+            'venue_address'          => 'new venue_address',
+            'city'                   => 'new city',
+            'state'                  => 'new state',
+            'zip'                    => '99999',
+            'ticket_price'           => '72.50',
+        ]);
+
+        $response->assertStatus(403);
+        tap($concert->fresh(), function($concert) {
+            $this->assertEquals('old title', $concert->title);
+            $this->assertEquals('old subtitle', $concert->subtitle);
+            $this->assertEquals('old additional_information', $concert->additional_information);
+            $this->assertEquals(Carbon::parse('2017-01-01 5:00pm'), $concert->date);
+            $this->assertEquals('old venue', $concert->venue);
+            $this->assertEquals('old venue_address', $concert->venue_address);
+            $this->assertEquals('old city', $concert->city);
+            $this->assertEquals('old state', $concert->state);
+            $this->assertEquals('00000', $concert->zip);
+            $this->assertEquals(2000, $concert->ticket_price);
+        });
+    }
 }
