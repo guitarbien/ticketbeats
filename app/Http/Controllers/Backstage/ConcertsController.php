1b5ece02e4fb7f24267af72892b163a88e5966f9
guitarbien <guitarbien@gmail.com>
2017-07-21 00:31:38 +0800

    實作改為撈登入使用者建立的音樂會

--- app/Http/Controllers/Backstage/ConcertsController.php
+++ app/Http/Controllers/Backstage/ConcertsController.php
@@ -68,7 +68,7 @@ class ConcertsController extends Controller
 
     public function update($id)
     {
-        $concert = Concert::findOrFail($id);
+        $concert = Auth::user()->concerts()->findOrFail($id);
 
         $concert->update([
             'title'                  => request('title'),
