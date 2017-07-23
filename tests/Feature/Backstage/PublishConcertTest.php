b6afecff716100be295cdf15f52901d613471a3c
guitarbien <guitarbien@gmail.com>
2017-07-23 23:07:26 +0800

    testcase：管理者可以發佈自己的音樂會
    不被rest的resource框架受限，建立新的controller取有意義的名字來實作

---
+++ tests/Feature/Backstage/PublishConcertTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Feature\Backstage;
+
+use App\Concert;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class PublishConcertTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_管理者可以發佈自己的音樂會()
+    {
+        $user = factory(User::class)->create();
+        $concert = factory(Concert::class)->states('unpublished')->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->actingAs($user)->post('/backstage/published-concerts', [
+            'concert_id' => $concert->id,
+        ]);
+
+        $response->assertRedirect('backstage/concerts');
+        $this->assertTrue($concert->fresh()->isPublished());
+    }
+}
