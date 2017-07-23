e1d416d17002bc515a9fb606e99d47d8534d693b
guitarbien <guitarbien@gmail.com>
2017-07-23 09:11:45 +0800

    (update database seeder)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,12 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class)->create([
+        $user = factory(User::class)->create([
             'email'    => 'bien@example.com',
             'password' => bcrypt('secret'),
         ]);
 
         factory(Concert::class)->states('published')->create([
+            'user_id' => $user->id,
             'title' => "The Red Chord",
             'subtitle' => "with Animosity and Lethargy",
             'venue' => "The Mosh Pit",
@@ -27,9 +28,23 @@ class DatabaseSeeder extends Seeder
             'city' => "Laraville",
             'state' => "ON",
             'zip' => "17916",
-            'date' => Carbon::parse('2016-12-13 8:00pm'),
+            'date' => Carbon::parse('2017-09-13 8:00pm'),
             'ticket_price' => 3250,
             'additional_information' => "This concert is 19+.",
+        ])->addTickets(10);
+
+        factory(Concert::class)->create([
+            'user_id' => $user->id,
+            'title' => "Slayer",
+            'subtitle' => "with Forbidden and Testament",
+            'venue' => "The Rock Pile",
+            'venue_address' => "55 Sample Blvd",
+            'city' => "Laraville",
+            'state' => "ON",
+            'zip' => "19276",
+            'date' => Carbon::parse('2017-10-05 7:00pm'),
+            'ticket_price' => 5500,
+            'additional_information' => null,
         ])->addTickets(10);
     }
 }
