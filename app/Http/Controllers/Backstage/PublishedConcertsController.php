bcc9a2edec2e63647bcfb59c7bd45d26fcc34949
guitarbien <guitarbien@gmail.com>
2017-07-29 15:19:12 +0800

    TestCase：管理者不能發佈別人的音樂會 音樂會改成發佈時應檢查登入

--- app/Http/Controllers/Backstage/PublishedConcertsController.php
+++ app/Http/Controllers/Backstage/PublishedConcertsController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Backstage;
 
 use App\Concert;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class PublishedConcertsController extends Controller
 {
     public function store()
     {
-        $concert = Concert::find(request('concert_id'));
+        $concert = Auth::user()->concerts()->findOrFail(request('concert_id'));
 
         if ($concert->isPublished()) {
             abort(422);
--- tests/Feature/Backstage/PublishConcertTest.php
+++ tests/Feature/Backstage/PublishConcertTest.php
@@ -48,4 +48,24 @@ class PublishConcertTest extends TestCase
         $this->assertEquals(3, $concert->fresh()->ticketsRemaining());
     }
 
+    public function test_管理者不能發佈別人的音樂會()
+    {
+        $user = factory(User::class)->create();
+        $otherUser = factory(User::class)->create();
+        $concert = factory(Concert::class)->states('unpublished')->create([
+            'user_id' => $otherUser->id,
+            'ticket_quantity' => 3,
+        ]);
+
+        $response = $this->actingAs($user)->post('/backstage/published-concerts', [
+            'concert_id' => $concert->id,
+        ]);
+
+        $response->assertStatus(404);
+
+        $concert = $concert->fresh();
+        $this->assertFalse($concert->isPublished());
+        $this->assertEquals(0, $concert->ticketsRemaining());
+    }
+
 }
