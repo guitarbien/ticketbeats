001cdd9ad1512bc4e2f4881300ea330a8fa1d683
guitarbien <guitarbien@gmail.com>
2017-07-31 22:07:07 +0800

    TestCase：管理者可以看到自己已發佈的音樂會的訂單
    紅燈

---
+++ tests/Feature/Backstage/ViewPublishedConcertOrdersTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Backstage;
+
+use App\User;
+use ConcertFactory;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class ViewPublishedConcertOrdersTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_管理者可以看到自己已發佈的音樂會的訂單()
+    {
+        $user = factory(User::class)->create();
+        $concert = ConcertFactory::createPublished(['user_id' => $user->id]);
+
+        $response = $this->actingAs($user)->get("/backstage/published-concerts/{$concert->id}/orders");
+
+        $response->assertStatus(200);
+        $response->assertEquals('backstage.published-concert-orders.index', $this->original->name());
+        $this->assertTrue($response->data('concert')->is('concert'));
+    }
+}
