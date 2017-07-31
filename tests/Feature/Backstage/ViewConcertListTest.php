623a5de3e4150f72d2a8a2a1a818f06b4f333792
guitarbien <guitarbien@gmail.com>
2017-07-31 23:18:01 +0800

    順便把之前的test改用新method

--- tests/Feature/Backstage/ViewConcertListTest.php
+++ tests/Feature/Backstage/ViewConcertListTest.php
@@ -54,9 +54,9 @@ class ViewConcertListTest extends TestCase
         $publishedConcertB = ConcertFactory::createPublished(['user_id' => $otherUser->id]);
         $publishedConcertC = ConcertFactory::createPublished(['user_id' => $user->id]);
 
-        $unpublishedConcertA = factory(Concert::class)->states('unpublished')->create(['user_id' => $user->id]);
-        $unpublishedConcertB = factory(Concert::class)->states('unpublished')->create(['user_id' => $otherUser->id]);
-        $unpublishedConcertC = factory(Concert::class)->states('unpublished')->create(['user_id' => $user->id]);
+        $unpublishedConcertA = ConcertFactory::createunPublished(['user_id' => $user->id]);
+        $unpublishedConcertB = ConcertFactory::createunPublished(['user_id' => $otherUser->id]);
+        $unpublishedConcertC = ConcertFactory::createunPublished(['user_id' => $user->id]);
 
         $response = $this->actingAs($user)->get('/backstage/concerts');
 
