a0f25da495fb0bcd6f94a53c68727b82796d2cef
guitarbien <guitarbien@gmail.com>
2017-08-09 21:46:07 +0800

    TestCase 全部的票券應包含所有票券狀態 計算總票數不用區分票券是否賣出

--- tests/unit/ConcertTest.php
+++ tests/unit/ConcertTest.php
@@ -88,6 +88,15 @@ class ConcertTest extends TestCase
         $this->assertEquals(3, $concert->ticketsSold());
     }
 
+    public function test_全部的票券應包含所有票券狀態()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->tickets()->saveMany(factory(Ticket::class, 3)->create(['order_id' => 1]));
+        $concert->tickets()->saveMany(factory(Ticket::class, 2)->create(['order_id' => null]));
+
+        $this->assertEquals(5, $concert->totalTickets());
+    }
+
     public function test_保留超過可購買的票券數量會拋出例外()
     {
         $concert = ConcertFactory::createPublished(['ticket_quantity' => 10]);
