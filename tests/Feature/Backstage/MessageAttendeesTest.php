9ea6f9ab0ad7647e25af64b5ad2f76315b0a0161
guitarbien <guitarbien@gmail.com>
2017-08-26 23:47:39 +0800

    TestCase：管理者可以發送訊息給所有參加者 管理者可以對自己的音樂會寫入 AttendeeMessage table
    測試失敗，404

--- tests/Feature/Backstage/MessageAttendeesTest.php
+++ tests/Feature/Backstage/MessageAttendeesTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Backstage;
 
+use App\Concert;
 use App\User;
 use ConcertFactory;
 use Tests\TestCase;
@@ -44,5 +45,29 @@ class MessageAttendeesTest extends TestCase
         $concert = ConcertFactory::createPublished();
         $response = $this->get("/backstage/concerts/{$concert->id}/messages/new");
         $response->assertRedirect('/login');
+    }
+
+    public function test_管理者可以發送訊息給所有參加者()
+    {
+        /** @var User $user */
+        $user    = factory(User::class)->create();
+
+        /** @var Concert $concert */
+        $concert = ConcertFactory::createPublished([
+            'user_id' => $user->id
+        ]);
+
+        $response = $this->actingAs($user)->post("/backstage/concerts/{$concert->id}/messages", [
+            'subject' => 'My subject',
+            'message' => 'My message',
+        ]);
+
+        $response->assertRedirect("/backstage/concerts/{$concert->id}/messages/new");
+        $response->assertSessionHas('flash');
+
+        $message = AttendeeMessage::first();
+        $this->assertEquals($concert->id, $message->concert_id);
+        $this->assertEquals('My subject', $message->subject);
+        $this->assertEquals('My message', $message->message);
     }
 }
