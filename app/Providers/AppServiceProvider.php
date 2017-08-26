2d0b84950f8e44edece1c205074eb9caceba4fac
guitarbien <guitarbien@gmail.com>
2017-08-26 20:16:39 +0800

    register the LaravelIdeHelper in local and testing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,6 +32,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment('local', 'testing')) {
             $this->app->register(DuskServiceProvider::class);
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
 
         $this->app->bind(StripePaymentGateway::class, function() {
