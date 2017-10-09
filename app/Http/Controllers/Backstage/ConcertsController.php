f6d3f87ba23901516074fc4e00b9750977cf41c9
guitarbien <guitarbien@gmail.com>
2017-10-09 19:09:19 +0800

    因預期此 null class 會常用到，所以建立實體檔案 測試通過

--- app/Http/Controllers/Backstage/ConcertsController.php
+++ app/Http/Controllers/Backstage/ConcertsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backstage;
 
 use App\Concert;
+use App\NullFile;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -55,12 +56,7 @@ class ConcertsController extends Controller
             'zip'                    => request('zip'),
             'ticket_price'           => request('ticket_price') * 100,
             'ticket_quantity'        => (int) request('ticket_quantity'),
-            'poster_image_path'      => request('poster_image', new class {
-                public function store()
-                {
-                    return null;
-                }
-            })->store('posters', 's3'),
+            'poster_image_path'      => request('poster_image', new NullFile)->store('posters', 's3'),
         ]);
 
         return redirect()->route('backstage.concerts.index');
