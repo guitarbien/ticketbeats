a38bd716d791193b4ae0e71b96f3e910e10e8e37
guitarbien <guitarbien@gmail.com>
2017-11-19 13:45:49 +0800

    test case 若poster_image存在則會產生一個queue_job來處理image
    error : job was not pushed

--- tests/Unit/Listeners/SchedulePosterImageProcessingTest.php
+++ tests/Unit/Listeners/SchedulePosterImageProcessingTest.php
@@ -2,18 +2,26 @@
 
 namespace Tests\Unit\Listeners;
 
+use App\Events\ConcertAdded;
+use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SchedulePosterImageProcessingTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+
+    public function test_若poster_image存在則會產生一個queue_job來處理image()
     {
-        $this->assertTrue(true);
+        Queue::fake();
+
+        $concert = \ConcertFactory::createUnpublished([
+            'poster_image_path' => 'posters/example-poster.png',
+        ]);
+
+        // It will push a job (ProcessPosterImage) to a queue when dispatch an event
+        ConcertAdded::dispatch($concert);
+
+        Queue::assertPushed(ProcessPosterImage::class);
     }
 }
