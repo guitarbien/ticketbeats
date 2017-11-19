25436073808b2520f3c4fd5f8bc383cfd7c812f8
guitarbien <guitarbien@gmail.com>
2017-11-19 17:28:33 +0800

    listener 收到 event 時 dispatch 一個 Job

--- app/Listeners/SchedulePosterImageProcessing.php
+++ app/Listeners/SchedulePosterImageProcessing.php
@@ -3,29 +3,17 @@
 namespace App\Listeners;
 
 use App\Events\ConcertAdded;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Jobs\ProcessPosterImage;
 
 class SchedulePosterImageProcessing
 {
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
     public function __construct()
     {
         //
     }
 
-    /**
-     * Handle the event.
-     *
-     * @param  ConcertAdded  $event
-     * @return void
-     */
     public function handle(ConcertAdded $event)
     {
-        //
+        ProcessPosterImage::dispatch($event);
     }
 }
