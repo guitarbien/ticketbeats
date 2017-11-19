7e47d743d308ccb6ca512827bfdb3d66321dc72e
guitarbien <guitarbien@gmail.com>
2017-11-19 13:34:52 +0800

    使用 artisan 建立新的 listeners test
    php artisan make:test --unit Listeners/SchedulePosterImageProcessingTest

---
+++ tests/Unit/Listeners/SchedulePosterImageProcessingTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit\Listeners;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SchedulePosterImageProcessingTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
