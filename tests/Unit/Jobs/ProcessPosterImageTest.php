5bb5ad20afa64ee8ff419b9a58df7d89ced4f8cf
guitarbien <guitarbien@gmail.com>
2017-12-17 16:31:10 +0800

    TestCase 改變posterImage寬度至600px
    在 tests 下建立 __fixtures__ 資料夾放測試所需檔案
    每次 Storage::fake() 時會將 storage 的資料清空
    再用 Storage::disk() 從 __fixtures__ 複製所需資訊過去

--- tests/Unit/Jobs/ProcessPosterImageTest.php
+++ tests/Unit/Jobs/ProcessPosterImageTest.php
@@ -2,10 +2,24 @@
 
 namespace Tests\Unit\Jobs;
 
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+/**
+ * Class ProcessPosterImageTest
+ * @package Tests\Unit\Jobs
+ */
 class ProcessPosterImageTest extends TestCase
 {
+    use RefreshDatabase;
 
+    public function test_改變posterImage寬度至600px()
+    {
+        Storage::fake('public');
+        Storage::disk('public')->put(
+            'posters/example-poster.png',
+            file_get_contents(base_path('tests/__fixtures__/full-size-poster.png'))
+        );
+    }
 }
