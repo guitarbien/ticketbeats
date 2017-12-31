cf4b00a0ab3e6e5b57e3e824c7ae723919fd3f97
guitarbien <guitarbien@gmail.com>
2017-12-31 19:50:14 +0800

    model 加入 findByCode()
    測試通過

--- app/Invitation.php
+++ app/Invitation.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invitation extends Model
 {
-    //
+    public static function findByCode(string $code)
+    {
+        return self::where('code', $code)->first();
+    }
 }
