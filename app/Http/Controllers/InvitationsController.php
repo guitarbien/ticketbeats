bd83f5e7c27bbd286855bcecaadd0f84c0193c40
guitarbien <guitarbien@gmail.com>
2017-12-31 19:48:16 +0800

    加入變數 invitation
    controller 從 get 取得 invitation code
    測試失敗 model findByCode() 不存在

--- app/Http/Controllers/InvitationsController.php
+++ app/Http/Controllers/InvitationsController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Invitation;
 use Illuminate\Http\Request;
 
 class InvitationsController extends Controller
 {
-    public function show()
+    public function show(string $code)
     {
-        return view('invitations.show');
+        $invitation = Invitation::findByCode($code);
+
+        return view('invitations.show', [
+            'invitation' => $invitation,
+        ]);
     }
 }
