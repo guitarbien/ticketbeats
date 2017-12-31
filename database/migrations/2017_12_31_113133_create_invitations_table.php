27fd13995dd6a8f2939081987237cb9f96284bc2
guitarbien <guitarbien@gmail.com>
2017-12-31 19:33:25 +0800

    建立 code 欄位
    測試失敗，endpoint 不存在

--- database/migrations/2017_12_31_113133_create_invitations_table.php
+++ database/migrations/2017_12_31_113133_create_invitations_table.php
@@ -15,6 +15,7 @@ class CreateInvitationsTable extends Migration
     {
         Schema::create('invitations', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('code');
             $table->timestamps();
         });
     }
