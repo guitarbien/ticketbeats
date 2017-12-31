7c4d044caf6225f5b1f67750070d57d7172fdd4f
guitarbien <guitarbien@gmail.com>
2017-12-31 20:07:34 +0800

    migration 加入 nullable()
    測試通過

--- database/migrations/2017_12_31_113133_create_invitations_table.php
+++ database/migrations/2017_12_31_113133_create_invitations_table.php
@@ -15,7 +15,7 @@ class CreateInvitationsTable extends Migration
     {
         Schema::create('invitations', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('user_id')->nullable();
             $table->string('code');
             $table->timestamps();
         });
