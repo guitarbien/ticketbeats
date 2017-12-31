190dc852ea087efbb53b681581d0219a2a2b1295
guitarbien <guitarbien@gmail.com>
2017-12-31 19:24:12 +0800

    test case 看到一個尚未使用的邀請
    call api 會回傳一個含有邀請碼 TESTCODE1234的頁面
    測試失敗 還沒建立 Invitation factory

--- tests/Feature/AcceptInvitationTest.php
+++ tests/Feature/AcceptInvitationTest.php
@@ -8,4 +8,16 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AcceptInvitationTest extends TestCase
 {
+    public function test_看到一個尚未使用的邀請()
+    {
+        $invitation = factory(Invitation::class)->create([
+            'code' => 'TESTCODE1234',
+        ]);
+
+        $response = $this->get('/invitations/TESTCODE1234');
+
+        $response->assertStatus(200);
+        $response->assertViewIs('invitations.show');
+        $this->assertTrue($response->data('invitation')->is($invitation));
+    }
 }
