fcf2780ae2e95109aecc8ddaf48458ca2a951f6c
guitarbien <guitarbien@gmail.com>
2018-01-01 19:15:23 +0800

    InvitationEmail 加入 property and construct parameter

--- app/Mail/InvitationEmail.php
+++ app/Mail/InvitationEmail.php
@@ -2,23 +2,29 @@
 
 namespace App\Mail;
 
+use App\Invitation;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
+/**
+ * Class InvitationEmail
+ * @package App\Mail
+ */
 class InvitationEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $invitation;
+
     /**
-     * Create a new message instance.
-     *
-     * @return void
+     * InvitationEmail constructor.
+     * @param Invitation $invitation
      */
-    public function __construct()
+    public function __construct(Invitation $invitation)
     {
-        //
+        $this->invitation = $invitation;
     }
 
     /**
