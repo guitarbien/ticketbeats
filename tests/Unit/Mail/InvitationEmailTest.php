2a36c618cba09866de110bfdc7377cf7301698d9
guitarbien <guitarbien@gmail.com>
2018-01-01 19:28:49 +0800

    php artisan make:test Mail/InvitationEmailTest --unit

---
+++ tests/Unit/Mail/InvitationEmailTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Unit\Mail;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InvitationEmailTest extends TestCase
+{
+}
