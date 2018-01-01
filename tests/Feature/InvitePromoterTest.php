f3baa3c4788aadefc95a602a17ce5edb6bc31bef
guitarbien <guitarbien@gmail.com>
2018-01-01 18:35:47 +0800

    test case 使用cli邀請promoter
    invitation code should be unique, just like order confirmation code
    測試失敗 InvitationCode 不存在

--- tests/Feature/InvitePromoterTest.php
+++ tests/Feature/InvitePromoterTest.php
@@ -2,10 +2,24 @@
 
 namespace Tests\Feature;
 
+use App\Invitation;
+use App\Facades\InvitationCode;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class InvitePromoterTest extends TestCase
 {
+    public function test_使用cli邀請promoter()
+    {
+        InvitationCode::shouldReceive('generate')->andReturn('TESTCODE1234');
+
+        $this->artisan('invite-promoter', ['email' => 'john@example.com']);
+
+        $this->assertEquals(1, Invitation::count());
+
+        $invitation = Invitation::first();
+        $this->assertEquals('john@example.com', $invitation->email);
+        $this->assertEquals('TESTCODE1234', $invitation->code);
+    }
 }
