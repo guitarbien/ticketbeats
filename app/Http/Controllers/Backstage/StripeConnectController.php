eb98a4760688e7232cc889312c8a32a8f2e7efb0
guitarbien <guitarbien@gmail.com>
2018-01-21 19:47:05 +0800

    以 Zttp 實作呼叫 stripe endpoint，再將取回的 user_id 和 token 更新回 user 資料表
    調整預設的 user model，讓資料可以寫入
    測試通過

--- app/Http/Controllers/Backstage/StripeConnectController.php
+++ app/Http/Controllers/Backstage/StripeConnectController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Backstage;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Zttp\Zttp;
 
 /**
  * Class StripeConnectController
@@ -33,6 +35,17 @@ class StripeConnectController extends Controller
      */
     public function redirect()
     {
+        $accessTokenResponse = Zttp::asFormParams()-> post('https://connect.stripe.com/oauth/token', [
+            'grant_type'    => 'authorization_code',
+            'code'          => request('code'),
+            'client_secret' => config('services.stripe.secret'),
+        ])->json();
+
+        Auth::user()->update([
+            'stripe_account_id'   => $accessTokenResponse['stripe_user_id'],
+            'stripe_access_token' => $accessTokenResponse['access_token'],
+        ]);
+
         return redirect()->route('backstage.concerts.index');
     }
 }
