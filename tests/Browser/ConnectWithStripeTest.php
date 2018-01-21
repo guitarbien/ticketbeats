4bae916783a8a4bdb615930975d7fe5e3de446d3
guitarbien <guitarbien@gmail.com>
2018-01-21 13:00:20 +0800

    test case 可以成功連結stripe_account

--- tests/Browser/ConnectWithStripeTest.php
+++ tests/Browser/ConnectWithStripeTest.php
@@ -2,22 +2,37 @@
 
 namespace Tests\Browser;
 
+use App\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
+/**
+ * Class ConnectWithStripeTest
+ * @package Tests\Browser
+ */
 class ConnectWithStripeTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
     /**
-     * A Dusk test example.
-     *
-     * @return void
+     * @throws \Exception
+     * @throws \Throwable
      */
-    public function testExample()
+    public function test_可以成功連結stripe_account()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+        $user = factory(User::class)->create([
+            'stripe_account_id'   => null,
+            'stripe_access_token' => null,
+        ]);
+
+        $this->browse(function (Browser $browser) use($user) {
+            $browser->loginAs($user)
+                    ->visit('/backstage/stripe-connect/authorize')
+                    ->assertUrlIs('https://connect.stripe.com/oauth/authorize')
+                    ->assertQueryStringHas('response_type', 'code')
+                    ->assertQueryStringHas('scope', 'read_write')
+                    ->assertQueryStringHas('client_id', config('services.stripe.client_id'));
         });
     }
 }
