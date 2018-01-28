4fbd129181be2606629663a4c4f483144d9edc2d
guitarbien <guitarbien@gmail.com>
2018-01-28 15:39:40 +0800

    建立 unit test
    php artisan make:test Http/Middleware/ForceStripeAccountTest --unit

---
+++ tests/Unit/Http/Middleware/ForceStripeAccountTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Http\Middleware;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+/**
+ * Class ForceStripeAccountTest
+ * @package Tests\Unit\Http\Middleware
+ */
+class ForceStripeAccountTest extends TestCase
+{
+    use RefreshDatabase;
+}
