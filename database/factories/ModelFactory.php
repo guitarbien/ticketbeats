0ac8bae15907203ffee6d47e2b07a387d0146627
guitarbien <guitarbien@gmail.com>
2018-05-05 00:36:43 +0800

    ModelFactory addUser 加上 stripe account 和 token 兩個欄位
    所有測試通過

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,6 +21,8 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'stripe_account_id' => 'test_acct_1234',
+        'stripe_access_token' => 'test_token',
     ];
 });
 
