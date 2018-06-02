35627517a1cf18a95d05e7191c61be7e398af152
guitarbien <guitarbien@gmail.com>
2018-06-02 23:29:36 +0800

    TestCase 款項的90%會被轉給 promoter
    金額部分成功

--- tests/Unit/Billing/StripePaymentGatewayTest.php
+++ tests/Unit/Billing/StripePaymentGatewayTest.php
@@ -16,4 +16,18 @@ class StripePaymentGatewayTest extends TestCase
     {
         return new StripePaymentGateway(config('services.stripe.secret'));
     }
+
+    public function test_90趴的付款會被轉給DestinationAccountId()
+    {
+        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
+
+        $paymentGateway->charge(5000, $paymentGateway->getValidTestToken(), env('STRIPE_TEST_PROMOTER_ID'));
+
+        $lastStripeCharge = array_first(\Stripe\Charge::all(
+            ["limit" => 1],
+            ['api_key' => config('services.stripe.secret')]
+        )->data);
+
+        static::assertEquals(5000, $lastStripeCharge['amount']);
+    }
 }
