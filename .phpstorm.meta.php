636428cb90c149085f57803a88c73b302a56ded5
guitarbien <guitarbien@gmail.com>
2019-09-21 13:22:38 +0800

    composer require --dev sebastian/phpcpd

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-05-16 14:04:31.
+    * Generated on 2019-09-21 05:22:25.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.2 on 2019-05-16 14:04:28.
+ * Generated for Laravel 5.8.2 on 2019-09-21 05:22:23.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
