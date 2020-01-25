75df9339ca35f8af672621dfddbe281a9201dd3f
guitarbien <guitarbien@gmail.com>
2020-01-25 14:59:29 +0800

    chore: composer remove --dev satooshi/php-coveralls

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-01-06 15:37:38.
+    * Generated on 2020-01-25 06:59:06.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.36 on 2020-01-06 15:37:38.
+ * Generated for Laravel 5.8.36 on 2020-01-25 06:59:06.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
