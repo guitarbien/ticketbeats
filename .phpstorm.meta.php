744772dea4bb757c355780a2b5386e9863dbf99e
guitarbien <guitarbien@gmail.com>
2020-03-08 14:42:56 +0800

    chore: upgrade phpunit-result-printer

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-08 05:54:40.
+    * Generated on 2020-03-08 06:42:29.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
